c8ef8485fb44d53d56ba9959cab5666f290dc590
Alpha-4 <aarondadhwal@gmail.com>
2025-08-24 15:47:12 +0530

    initial setup with create and add functionality

---
+++ tests/mycalculatordapp.ts
@@ -0,0 +1,40 @@
+import * as anchor from "@coral-xyz/anchor";
+//import { Program } from "@coral-xyz/anchor";
+//import { Mycalculatordapp } from "../target/types/mycalculatordapp";
+import { SystemProgram } from "@solana/web3.js";
+import {assert} from "chai";
+
+// js imports
+// const assert = require('assert');
+// const anchor = require('@coral-xyz/anchor');
+// const {SystemProgram} = anchor.web3;
+
+describe("mycalculatordapp",()=>{
+    const provider= anchor.getProvider();
+    anchor.setProvider(provider);
+    const calculator=anchor.web3.Keypair.generate();
+    const program=anchor.workspace.Mycalculatordapp;
+
+    it("creates a calculator dapp", async ()=>{
+        await program.rpc.create("welcome to solana",{
+            accounts:{
+                calculator: calculator.publicKey,
+                user: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId,
+            },
+            signers:[calculator]
+        });
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.greeting == "welcome to solana")
+    })
+
+    it("perform add operation on a calculator dapp", async ()=>{
+        await program.rpc.add(new anchor.BN(2), new anchor.BN(3),{
+            accounts:{
+                calculator: calculator.publicKey,
+            }
+        });
+        const account = await program.account.calculator.fetch(calculator.publicKey);
+        assert.ok(account.result.eq(new anchor.BN(5)));
+    })
+})
